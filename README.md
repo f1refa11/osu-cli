6621bc4f47695b080bedad5eb08441070652ee02
FireFall <firekonstantin@mail.ru>
2023-03-06 19:21:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# osu-cli
+ Minimalistic osu! terminal-based beatmap downloader, written in Python using ossapi
---
+++ main.py
@@ -0,0 +1,42 @@
+import sys,pprint
+nocolor = True
+if not "-nc" in sys.argv:
+    print("Importing colorama...")
+    import colorama
+    nocolor = True
+print("Imporing os..")
+import os
+print("Getting console size...")
+ch = os.get_terminal_size().lines - 2
+os.system('cls')
+helpListSrc = [
+    "'new' - show new maps",
+    "'du [url]' - download via url",
+    "'dl [num]' - download from current list",
+    "'di [id]' - download by id",
+    "'ds' - download selected beatmap(use sel)",
+    "'sel [num]' - select beatmap from the current list",
+    "'selid [id]' - select beatmap by id",
+    "'selurl [url] - select beatmap via url",
+    "'p [num]' - switch to the page",
+    "'setup' - configure osu! directory",
+]
+helpList = [helpListSrc[i:i + ch] for i in range(0, len(helpListSrc), ch)]
+print("""
+\n
+osu!dl - osu! download client and osu!direct alternative
+'h' to show all commands, 'x' to exit
+\n""")
+while 1:
+    c = input("> ").split()
+    os.system('cls')
+    if c[0] == "h":
+        if len(c) == 1:
+            for x in helpList[0]:
+                print(x)
+            print("Page 1/%s. Use 'h [num]' to switch to another page"%len(helpList))
+        else:
+            for x in helpList[c[1]]:
+                print()
+    elif c[0] == "x":
+        break
